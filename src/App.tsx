41456f4f385e249de5ff0b0d228e16870f195ef7
Edjery Gumbao <edisgumbao@gmail.com>
2025-09-09 11:39:27 +0800

    init: material ui and header title

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { AppBar, Toolbar, Typography, Box } from '@mui/material';
+// import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+
 function App() {
   return (
-    <>
-      <p>Hello World</p>
-    </>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{ backgroundColor: '#1976d2' }}>
+        <Toolbar>
+          {/* <AccountBalanceWalletIcon sx={{ mr: 2 }} /> */}
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Budget Trackr
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box sx={{ p: 3 }}>
+        {/* Main content will go here */}
+      </Box>
+    </Box>
+  );
 }
 
-export default App
+export default App;
